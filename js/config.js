69b9ad17b0e5a4d7083789c700c54915c581cbb2
harrisonmiller <harrison918@aol.com>
2020-09-03 17:16:31 -0400

    updated credentials for sample view testing

--- js/config.js
+++ js/config.js
@@ -7,17 +7,18 @@ const SERVERS = [
   {name:"Albany Stage", url:"https://kf6-stage.rit.albany.edu/"},
   {name:"IKIT", url:"https://kf6.ikit.org/"},
   {name:"Albany", url:"https://kf6.rit.albany.edu/"},
-  {name:"Singapore", url:"https://kf.rdc.nie.edu.sg/"}
+  {name:"Singapore", url:"https://kf.rdc.nie.edu.sg/"},
+  {name:"Local", url:"http://localhost:9000/"}
 ];
 
 /*
 * EDIT VALUES FOR iFRAME SAMPLE VIEW HERE
 */
-const USERNAME = "demo1";
-const PASSWORD = "demo1";
-const SERVER = getServerURL("IKIT Stage");
-const WELCOMEVIEWID = "558abcb01f3b621e75d9bc0a"; // PKU SUMMER SCHOOL COMMUNITY 
-
+const USERNAME = "admin";
+const PASSWORD = "build";
+const SERVER = getServerURL("Local");
+const WELCOMEVIEWID = "5f5009ec1beff90212b92dd6"; // TEST COMMUNITY WELCOME VIEW ID
+const COMMUNITYID = "5f5009eb1beff90212b92dd4" // TEST COMMUNITY ID
 
 // Helper function to retrieve a servers name from its URL
 function getServerName(url){
--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
     return false;
   })
 
-  getiFrameSrc();
+  //getiFrameSrc();
 
 });
 
