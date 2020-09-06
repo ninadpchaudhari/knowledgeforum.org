9a410a6b02df6f616e1f852a175c65e68e3bec44
harrisonmiller <harrison918@aol.com>
2020-09-06 10:09:24 -0400

    added author and date to notes, npm added

--- js/demoview.js
+++ js/demoview.js
@@ -1,17 +1,26 @@
 $(document).ready(function() {
 
+  var cytoscape = require('cytoscape');
+  var nodeHtmlLabel = require('cytoscape-node-html-label');
+  nodeHtmlLabel( cytoscape ); // register extension
+
   // initialize the graph
   var cy = cytoscape({
     container: document.getElementById('cy'), // container to render in
-
     elements: [],
-
     style: [ // the stylesheet for the graph
       {
         selector: 'node',
         style: {
           'background-color': '#6ba6d6',
-          'label': 'data(name)'
+          'label': "data(name)"
+        }
+      },
+
+      {
+        selector: '.readNode',
+        style: {
+          'background-color': '#E87259',
         }
       },
 
@@ -35,30 +44,48 @@ $(document).ready(function() {
     maxZoom: 2,
   });
 
+  // set nodeHtmlLabel for your Cy instance
+  cy.nodeHtmlLabel([
+    {
+      query: 'node',
+      halign: 'center', // title vertical position. Can be 'left',''center, 'right'
+      valign: 'bottom', // title vertical position. Can be 'top',''center, 'bottom'
+      halignBox: 'center', // title vertical position. Can be 'left',''center, 'right'
+      valignBox: 'bottom', // title relative box vertical position. Can be 'top',''center, 'bottom'
+      cssClass: '', // any classes will be as attribute of <div> container for every title
+      tpl: function(data){
+        return '<div class = "cytoscape-label">' + data.author + '<br>' + data.date + '</div>';
+      }
+    }
+  ]);
 
-  // get demo user token
+
+  // get demo user token and build graph
   var getTokenPromise = createDemoUserTokenPromise();
   getTokenPromise.then(function(result) {
     var token = result[0].token;
 
-    // use token to get all the notes from the welcome view
     var promise = getApiLinksFromViewId(token, SERVER, WELCOMEVIEWID);
-    // use token to get all the edges for the welcome view
     var promise1 = postApiLinksCommunityIdSearch(token, SERVER, COMMUNITYID, {type: "buildson"});
+    var promise2 = getApiLinksReadStatus(token, SERVER, COMMUNITYID, WELCOMEVIEWID);
+    var promise3 = getCommunityAuthors(token, SERVER, COMMUNITYID);
 
-
-    Promise.all([promise, promise1]).then((result) => {
-      console.log(result);
+    Promise.all([promise, promise1, promise2, promise3]).then((result) => {
 
       // first add the notes to the graph
       for(var i = 0; i < result[0].length; i++){
         if(result[0][i]._to.type === "Note" && result[0][i]._to.title !== "" && result[0][i]._to.status === "active"){
-          console.log(result[0][i].to);
+          var authorName = matchAuthorId(result[0][i]._to.authors[0], result[3]);
+          var date = parseDate(result[0][i].created);
+          var className = result[2].includes(result[0][i].to) ? 'readNode' : 'node';
           cy.add({
               data: {
                 id: result[0][i].to,
-                name: result[0][i]._to.title
+                name: result[0][i]._to.title,
+                author: authorName,
+                date: date,
               },
+              classes: className,
               position: {
                 x: result[0][i].data.x,
                 y: result[0][i].data.y
@@ -67,7 +94,7 @@ $(document).ready(function() {
         }
       }
 
-      // second add the edges to the graph
+      // second add the edges
       for(var i = 0; i < result[1].length; i++){
         var obj = result[1][i];
         if(obj._to.type === "Note" && obj._to.status === "active" && obj._to.title != "" && obj._from.type === "Note" && obj._from.status === "active" && obj._from.title != ""){
@@ -86,7 +113,6 @@ $(document).ready(function() {
   });
 
 });
-
 
 
 // creates a promise for the demo users token
@@ -133,23 +159,47 @@ function getApiLinksFromViewId(token, server, welcomeViewId) {
 }
 
 
+// gets the data for read status of the notes
+// RETURNS ARRAY OF THE READ NOTE IDS ONLY
+function getApiLinksReadStatus(token, server, communityId, welcomeViewId){
+
+  return fetch(server + 'api/records/myreadstatusview/' + communityId + '/' + welcomeViewId, {
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + token
+    },
+  }).then(function(response) {
+    return response.json();
+  }).then(function(body) {
+    var results = [];
+    for(var i in body){
+      if(body[i].type == "read"){ results.push(body[i].to); }
+    }
+    return (results);
+  }).catch(function(error) {
+    return ("Error:", error);
+  });
+
+}
+
+
 // uses serverurl/api/objects/objectId endpoint
 function getApiObjectsObjectId(token, server, objectId) {
-    var request = new XMLHttpRequest();
 
-    request.open('GET', server + 'api/objects/' + objectId);
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.setRequestHeader('Authorization', 'Bearer ' + token);
-
-    request.onreadystatechange = function() {
-      if (this.readyState === 4) {
-        // console.log('Status:', this.status);
-        // console.log('Headers:', this.getAllResponseHeaders());
-        // console.log('Body:', this.responseText);
-      }
-    };
-
-    request.send();
+  return fetch(server + 'api/objects/' + objectId, {
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + token
+    },
+  }).then(function(response) {
+    return response.json();
+  }).then(function(body) {
+    return (body);
+  }).catch(function(error) {
+    return ("Error:", error);
+  });
 }
 
 
@@ -174,4 +224,48 @@ function postApiLinksCommunityIdSearch(token, server, communityId, query) {
     return ("Error:", error);
   });
 
+}
+
+
+function getCommunityAuthors(token, server, communityId) {
+
+  return fetch(server + 'api/communities/' + communityId + '/authors', {
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + token
+    },
+  }).then(function(response) {
+    return response.json();
+  }).then(function(body) {
+    return (body);
+  }).catch(function(error) {
+    return ("Error:", error);
+  });
+}
+
+
+// helper function to find an authors name from their author id using the getCommunityAuthors data
+function matchAuthorId(authorId, authorsInfo){
+    for(var i = 0; i < authorsInfo.length; i++){
+      if(authorId == authorsInfo[i]._id){
+        return authorsInfo[i].firstName + " " + authorsInfo[i].lastName;
+      }
+    }
+    return "Author not found";
+}
+
+// helper function to return formatted creation date of a note
+function parseDate(date){
+  var year = date.substring(0, 4);
+  var month = date.substring(5, 7);
+  var day = date.substring(8, 10);
+  var hour = date.substring(11, 13);
+  var minute = date.substring(14, 16);
+  var second = date.substring(17, 19);
+  if(hour < 12){
+    return(month + "/" + day + "/" + year + ", " + hour + ":" + minute + ":" + second + " AM");
+  } else {
+    return(month + "/" + day + "/" + year + ", " + (hour - 12) + ":" + minute + ":" + second + " PM");
+  }
 }
