d4ac0d8cf7b8ad95252f45a4f14a42c589590a16
harrisonmiller <harrison918@aol.com>
2021-04-20 12:13:48 -0400

    Added distinction between specific view settings and community settings for manager accounts

--- src/components/ViewSettingsPopover.jsx
+++ src/components/ViewSettingsPopover.jsx
@@ -11,17 +11,16 @@ class ViewSettingsPopover extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      viewSettingsObj: (this.props.thisViewsViewSettingsObj !== null) ? (this.props.thisViewsViewSettingsObj) :
-                            (this.props.communityViewSettingsObj !== null ? this.props.communityViewSettingsObj : {buildson: true, language: false, references: false, showAuthor: true, showGroup: false, showTime: true}),
-      communityViewSettingsObj: (this.props.thisViewsViewSettingsObj !== null) ? (this.props.thisViewsViewSettingsObj) :
-                            (this.props.communityViewSettingsObj !== null ? this.props.communityViewSettingsObj : {buildson: true, language: false, references: false, showAuthor: true, showGroup: false, showTime: true}),
+      tempViewSettingObj: {buildson: true, language: false, references: false, showAuthor: true, showGroup: false, showTime: true},
+      thisViewsViewSettingObj: {buildson: false, language: false, references: false, showAuthor: false, showGroup: false, showTime: false},
+      communityViewSettingsObj: {buildson: false, language: false, references: false, showAuthor: false, showGroup: false, showTime: false}
     }
 
     this.initializeSettings = this.initializeSettings.bind(this);
     this.handleViewTypeChange = this.handleViewTypeChange.bind(this);
     this.handleViewSettingsChange = this.handleViewSettingsChange.bind(this);
-    this.handleCommunitySettingsChange = this.handleCommunitySettingsChange.bind(this);
-    this.compareViewSettingsObjs = this.compareViewSettingsObjs.bind(this);
+    this.pushViewSettingsChange = this.pushViewSettingsChange.bind(this);
+    this.pushCommunitySettingsChange = this.pushCommunitySettingsChange.bind(this);
   }
 
   componentDidUpdate(prevProps, prevState){
@@ -30,32 +29,34 @@ class ViewSettingsPopover extends Component {
     }
   }
 
-  // used to reset the viewSettingsObj and communityViewSettingsObj state
-  // viewSettingsObj corresponds to the temp view settings and is always initiliazed to be the same as the community view settings
-  // communityViewSettingsObj is set to these values in this priority
-  // 1. The view objects viewSetting object if it exists
-  // 2. The community context objects viewSetting object if it exists
-  // 3. A default value of {buildson: true, language: false, references: false, showAuthor: true, showGroup: false, showTime: true}
+  // used to set initial values for each of the viewSettingObj states
   initializeSettings(){
+    // assign values for this specific view and the community settings if they exist
+    if(this.props.thisViewsViewSettingObj !== null){ this.setState({thisViewsViewSettingObj: this.props.thisViewsViewSettingObj}); }
+    if(this.props.communityViewSettingsObj !== null){ this.setState({communityViewSettingsObj: this.props.communityViewSettingsObj}); }
+
+    // set the current view setting values (tempViewSettingObj) to the settings
+    // provided in this priority ---> 1. this specific views viewSetting obj 2. the community context viewSetting obj 3. a default set of values
     var temp = {}
     if(this.props.thisViewsViewSettingObj !== null){
-      temp = this.props.thisViewsViewSettingsObj;
+      temp = this.props.thisViewsViewSettingObj;
     } else if(this.props.communityViewSettingsObj !== null){
       temp = this.props.communityViewSettingsObj;
     } else {
-      temp = {buildson: true, language: false, references: false, showAuthor: true, showGroup: false, showTime: true}
+      temp = {buildson: true, language: false, references: false, showAuthor: true, showGroup: false, showTime: true};
     }
-
-    this.setState({viewSettingsObj: temp, communityViewSettingsObj: temp});
+    this.setState({tempViewSettingObj: temp});
     this.props.setCurrViewSettingsObj(temp);
   }
 
+  // if the selected view type is different from the current - swap the view
   handleViewTypeChange(e){
     if(e.target.value !== this.props.currentView) this.props.switchView();
   }
 
-  // handles updating the viewSettingsObj according to the checked values in the popover on the sidebar
+  // a unified function that can update any of the viewSetting states
   handleViewSettingsChange(e){
+    // create the new viewSetting object from the target and list siblings
     var viewSettings = {};
     const domElem = e.target.parentNode;
     const siblings = $(domElem).siblings();
@@ -64,37 +65,31 @@ class ViewSettingsPopover extends Component {
       viewSettings[siblings[i].childNodes[1].name] = siblings[i].childNodes[1].checked;
     }
 
-    if(domElem.parentNode.id === "viewSettingsPopoverList"){
-      this.setState({viewSettingsObj: viewSettings});
+    // depending on id of the list update the corresponding state
+    if(domElem.parentNode.id === "tempSettingsPopoverList"){
+      this.setState({tempViewSettingObj: viewSettings});
       this.props.setCurrViewSettingsObj(viewSettings);
+    } else if(domElem.parentNode.id === "viewSettingsPopoverList"){
+      this.setState({thisViewsViewSettingObj: viewSettings});
     } else if(domElem.parentNode.id === "communitySettingsPopoverList"){
       this.setState({communityViewSettingsObj: viewSettings});
     }
   }
 
-  handleCommunitySettingsChange(){
-    // will either update the viewSettings in the view object or in the community context object
-    var viewSetting = this.state.communityViewSettingsObj;
-    var temp = {};
-    if(this.props.thisViewsViewSettingObj !== null || this.props.communityViewSettingsObj === null){
-      temp = JSON.parse(JSON.stringify(this.props.view));
-      temp.data.viewSetting = Object.assign({}, viewSetting);
-      this.props.setView(temp);
-      // add api call to update view object on back end here
-    } else if(this.props.communityViewSettingsObj !== null) {
-      temp = JSON.parse(JSON.stringify(this.props.communitySettings));
-      temp.data.viewSetting = Object.assign({}, viewSetting);
-      this.props.setCommunitySettings(temp);
-      // add api call to update community context object on back end here
-    }
+  // updates this specific views settings in redux and on the backend
+  pushViewSettingsChange(){
+    var newObject = JSON.parse(JSON.stringify(this.props.view));
+    newObject.data.viewSetting = Object.assign({}, this.state.thisViewsViewSettingObj);
+    this.props.setView(newObject);
+    console.log(newObject);
   }
 
-  // returns a deep comparison of two viewSetting objects
-  compareViewSettingsObjs(a, b){
-    if(a === null || b === null) return false;
-
-    return a.buildson === b.buildson && a.language === b.language && a.references === b.references
-                && a.showAuthor === b.showAuthor && a.showGroup === b.showGroup && a.showTime === b.showTime;
+  // updates this communitys context setting object in redux and on the backend
+  pushCommunitySettingsChange(){
+    var newObject = JSON.parse(JSON.stringify(this.props.communitySettings));
+    newObject.data.viewSetting = Object.assign({}, this.state.communityViewSettingsObj);
+    this.props.setCommunitySettings(newObject);
+    console.log(newObject);
   }
 
   render() {
@@ -106,6 +101,8 @@ class ViewSettingsPopover extends Component {
           rootClose
           overlay={
             <Popover>
+
+              {/* VIEW TYPE SELECTION */}
               <Popover.Title>View Type</Popover.Title>
               <Popover.Content id="viewTypeContent">
                 <Row>
@@ -113,22 +110,45 @@ class ViewSettingsPopover extends Component {
                   <Col><Button value="Light" onClick={this.handleViewTypeChange} className={this.props.currentView==="Light" ? 'activeView' : ''}>Light</Button></Col>
                 </Row>
               </Popover.Content>
+              {/* END VIEW TYPE SELECTION */}
+
+              {/* TEMPORARY VIEW SETTINGS */}
               <Popover.Title>View Settings (Temporary)<a id="viewSettingsReset" onClick={this.initializeSettings}><i className="fas fa-undo-alt"></i></a></Popover.Title>
               <Popover.Content>
-                <ul id="viewSettingsPopoverList">
-                  <li>Buildson <input type="checkbox" name="buildson" onChange={this.handleViewSettingsChange} checked={this.state.viewSettingsObj.buildson}></input></li>
-                  <li>Reference <input type="checkbox" name="references" onChange={this.handleViewSettingsChange} checked={this.state.viewSettingsObj.references}></input></li>
-                  <li>Group <input type="checkbox" name="showGroup" onChange={this.handleViewSettingsChange} checked={this.state.viewSettingsObj.showGroup}></input></li>
-                  <li>Author <input type="checkbox" name="showAuthor" onChange={this.handleViewSettingsChange} checked={this.state.viewSettingsObj.showAuthor}></input></li>
-                  <li>Date <input type="checkbox" name="showTime" onChange={this.handleViewSettingsChange} checked={this.state.viewSettingsObj.showTime}></input></li>
+                <ul id="tempSettingsPopoverList" className="settingsPopoverList">
+                  <li>Buildson <input type="checkbox" name="buildson" onChange={this.handleViewSettingsChange} checked={this.state.tempViewSettingObj.buildson}></input></li>
+                  <li>Reference <input type="checkbox" name="references" onChange={this.handleViewSettingsChange} checked={this.state.tempViewSettingObj.references}></input></li>
+                  <li>Group <input type="checkbox" name="showGroup" onChange={this.handleViewSettingsChange} checked={this.state.tempViewSettingObj.showGroup}></input></li>
+                  <li>Author <input type="checkbox" name="showAuthor" onChange={this.handleViewSettingsChange} checked={this.state.tempViewSettingObj.showAuthor}></input></li>
+                  <li>Date <input type="checkbox" name="showTime" onChange={this.handleViewSettingsChange} checked={this.state.tempViewSettingObj.showTime}></input></li>
                 </ul>
               </Popover.Content>
+              {/* END TEMPORARY VIEW SETTINGS */}
+
+              {/* PERMANENT SETTINGS SPECIFIC TO THIS VIEW */}
               {(this.props.author && this.props.author.role === "manager") ? (
-                <Popover.Title>Community Settings<a id="saveCommunitySettingsIcon" onClick={this.handleCommunitySettingsChange}><i className="far fa-save"></i></a></Popover.Title>
+                <Popover.Title>View Settings (Permanent)<a className="saveCommunitySettingsIcon" onClick={this.pushViewSettingsChange}><i className="far fa-save"></i></a></Popover.Title>
               ) : null}
               {(this.props.author && this.props.author.role === "manager") ? (
                 <Popover.Content>
-                  <ul id="communitySettingsPopoverList">
+                  <ul id="viewSettingsPopoverList" className="settingsPopoverList">
+                    <li>Buildson <input type="checkbox" name="buildson" onChange={this.handleViewSettingsChange} checked={this.state.thisViewsViewSettingObj.buildson}></input></li>
+                    <li>Reference <input type="checkbox" name="references" onChange={this.handleViewSettingsChange} checked={this.state.thisViewsViewSettingObj.references}></input></li>
+                    <li>Group <input type="checkbox" name="showGroup" onChange={this.handleViewSettingsChange} checked={this.state.thisViewsViewSettingObj.showGroup}></input></li>
+                    <li>Author <input type="checkbox" name="showAuthor" onChange={this.handleViewSettingsChange} checked={this.state.thisViewsViewSettingObj.showAuthor}></input></li>
+                    <li>Date <input type="checkbox" name="showTime" onChange={this.handleViewSettingsChange} checked={this.state.thisViewsViewSettingObj.showTime}></input></li>
+                  </ul>
+                </Popover.Content>
+              ) : null}
+              {/* END PERMANENT SETTINGS SPECIFIC TO THIS VIEW */}
+
+              {/* PERMANENT COMMUNITY WIDE SETTINGS */}
+              {(this.props.author && this.props.author.role === "manager") ? (
+                <Popover.Title>Community Settings<a className="saveCommunitySettingsIcon" onClick={this.pushCommunitySettingsChange}><i className="far fa-save"></i></a></Popover.Title>
+              ) : null}
+              {(this.props.author && this.props.author.role === "manager") ? (
+                <Popover.Content>
+                  <ul id="communitySettingsPopoverList" className="settingsPopoverList">
                     <li>Buildson <input type="checkbox" name="buildson" onChange={this.handleViewSettingsChange} checked={this.state.communityViewSettingsObj.buildson}></input></li>
                     <li>Reference <input type="checkbox" name="references" onChange={this.handleViewSettingsChange} checked={this.state.communityViewSettingsObj.references}></input></li>
                     <li>Group <input type="checkbox" name="showGroup" onChange={this.handleViewSettingsChange} checked={this.state.communityViewSettingsObj.showGroup}></input></li>
@@ -137,6 +157,8 @@ class ViewSettingsPopover extends Component {
                   </ul>
                 </Popover.Content>
               ) : null}
+              {/* END PERMANENT COMMUNITY WIDE SETTINGS */}
+
             </Popover>
           }>
           <Button className="circle-button pad sidebar-btn">
@@ -153,7 +175,7 @@ const mapStateToProps = (state, ownProps) => {
         communitySettings: state.globals.communitySettings,
         view: state.globals.view,
         currViewSettingsObj: state.globals.currViewSettingsObj,
-        thisViewsViewSettingsObj: state.globals.thisViewsViewSettingsObj,
+        thisViewsViewSettingObj: state.globals.thisViewsViewSettingsObj,
         communityViewSettingsObj: state.globals.communityViewSettingsObj,
     }
 }
